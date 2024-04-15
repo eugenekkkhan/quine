7ea5f9e6618e9553dcb9fcccb81b11c5b3f8cc94
eugenekkkhan <eugenekhananov@yandex.ru>
2024-04-16 02:24:44 +0300

    23rd commit from quine-method repo

--- src/components/generalAlgorithm.jsx
+++ src/components/generalAlgorithm.jsx
@@ -1,28 +1,28 @@
 'use strict';
-import { hover } from '@testing-library/user-event/dist/hover';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Latex from 'react-latex-next';
 import 'katex/dist/katex.min.css';
 import Logo from '../logo.svg'
+import ImplicationMatrix from './implicationMatrix';
 
 const _ = require('lodash');
 
-const generalAlgorithm = (value, power) => {
+const GeneralAlgorithm = (props) => {
   let arrayValues = new Array(); 
-  let len = value.length;
+  let len = props.value.length;
 
-  for (let i = 0; i < value.length; i++) {
-    arrayValues.push(Number(value[i]));
+  for (let i = 0; i < props.value.length; i++) {
+    arrayValues.push(Number(props.value[i]));
   }
 
   function decimalToBinaryArray(N) {
     let localVar = (N >>> 0).toString(2);
-    while (localVar.length < power) {
+    while (localVar.length < props.power) {
       localVar = '0' + localVar;
     }
 
     var localArr = [];
-    for (let i = 0; i < power; i++) {
+    for (let i = 0; i < props.power; i++) {
       localArr.push(localVar[i]);
     }
 
@@ -36,13 +36,13 @@ const generalAlgorithm = (value, power) => {
   }
 
   var topSigns1 = [];
-  for (let i = 0; i < power; i++) {
+  for (let i = 0; i < props.power; i++) {
     topSigns1.push(`${i}`)
   }
 
   let xValuesArrayFOne = []
   for (let i = 0; i < len; i++) {
-    if(value[i] == 1) {
+    if(props.value[i] == 1) {
       let arr = [];
       for (let j = 0; j < xValuesArray[i].length; j++){
         arr.push(xValuesArray[i][j])
@@ -134,33 +134,7 @@ const generalAlgorithm = (value, power) => {
     }
     return result;
   }
-  function Absorption(a, b) {
-    let result = [];
-    let found;
-    let amount = 0; 
-    let i = 0;
-
-    let aCopy = _.cloneDeep(a);
-    let bCopy = _.cloneDeep(b)
-
-    while ((amount < 2) && (i < aCopy.length)) {
-      if (((aCopy[i] == "2") && (bCopy[i] != "2")) || ((bCopy[i] == "2") && (aCopy[i] != "2"))) {
-        amount++;
-        found = i;
-      } else if ((aCopy[i] != "2") && (bCopy[i] != "2") && (aCopy[i] != bCopy[i])) {
-        return result;
-      }
-      i++;
-    }
-    if (amount === 1) {
-      result = aCopy;
-      result[found] = "2";
-    }
-    return result;
-  }
   
-
-  // [[[1,0,0], false], [[1,0,0], false]]
   function Abbreviate(data, func) {
     let dataCopy = _.cloneDeep(data);
     while (true) {
@@ -178,11 +152,8 @@ const generalAlgorithm = (value, power) => {
               let object = [buf, false];
               let repeats = false;
               for (let x of h) {
-                console.log([x[0],object[0]])
                 if (JSON.stringify(x[0]) === JSON.stringify(object[0])) {
-                  
                   repeats = true;
-                  // break;
                 }
               }
               if (!repeats) {
@@ -210,7 +181,7 @@ const generalAlgorithm = (value, power) => {
     }
     return dataCopy;
   }
-//1110001000000011
+
   let abbreviatedArray = [];
 
   for (let i = 0; i < xValuesArrayFOne.length; i++) {
@@ -240,17 +211,17 @@ const generalAlgorithm = (value, power) => {
 
   let topSigns2 = [];
 
-  for (let i = 0; i < xValuesArray.length; i++) {
+  for (let i = 0; i < xValuesArrayFOne.length; i++) {
     let localStr = "";
-    for (let j = 0; j < xValuesArray[i].length; j++) {
-      if (xValuesArray[i][j] == "1") {
-        if (j == xValuesArray[i].length-1) {
+    for (let j = 0; j < xValuesArrayFOne[i].length; j++) {
+      if (xValuesArrayFOne[i][j] == "1") {
+        if (j == xValuesArrayFOne[i].length-1) {
           localStr += "x_"+j;
         } else {
           localStr += "x_"+j +"\\land ";
         }
       } else {
-        if (j == xValuesArray[i].length-1) {
+        if (j == xValuesArrayFOne[i].length-1) {
           localStr += "\\overline x_"+j;
         } else {
           localStr += "\\overline x_"+j +"\\land ";
@@ -287,6 +258,8 @@ const generalAlgorithm = (value, power) => {
     leftSigns2.push(localStr)
   }
 
+  
+
   return (
   <div className='container'>
     <div className='flex-2-div'>
@@ -298,7 +271,7 @@ const generalAlgorithm = (value, power) => {
             <td className='cell'>f</td>
           </tr>
           {xValuesArray.map((innerArray, index) => {
-            return <tr><td className='cell-fixed'>{index+1}</td>{innerArray.map(element => <td className={(element == 1) ? 'cell green-bg' : 'cell red-bg'}>{element}</td>)} <td className={(value[index]==1) ? 'cell green-bg' : 'cell red-bg'}>{value[index]}</td></tr>
+            return <tr><td className='cell-fixed'>{index+1}</td>{innerArray.map(element => <td className={(element == 1) ? 'cell green-bg' : 'cell red-bg'}>{element}</td>)} <td className={(props.value[index]==1) ? 'cell green-bg' : 'cell red-bg'}>{props.value[index]}</td></tr>
           })}
           
         </div>
@@ -321,28 +294,12 @@ const generalAlgorithm = (value, power) => {
 
     <br/>
 
-    <div className='matrix-outer'>
-      <div className='matrix'>
-          <tr>
-            <td className='cell2'>&nbsp;</td>
-            {topSigns2.map(element => <td className='cell2'><Latex>${element}$</Latex></td>)}
-            <td className='cell-fixed'>f</td>
-          </tr>
-            {leftSigns2.map((element) => {
-            return <tr>
-              <td className='cell'><Latex>${element}$</Latex></td>
+    <ImplicationMatrix topSigns2={topSigns2} leftSigns2={leftSigns2} xValuesArrayFOne={xValuesArrayFOne} postAbbreviatedArray={postAbbreviatedArray}/>
 
-            </tr>})}
-        </div>
-      </div>
-
-
-      <div>{postAbbreviatedArray.join('|')}</div>
-      <div>{xValuesArrayFOne.join('|')}</div>
   </div>
 
 
   )
 }
 
-export default generalAlgorithm
+export default GeneralAlgorithm
